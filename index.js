fd5150338b6106854ecb7669c3deca9b0c044c71
yonggoonoh <nohyg90@gmail.com>
2018-11-21 22:55:40 +0900

    Print out test message

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * this file is for testing
+ */
+const uglify = require('./sender/uglify.js');
+
+const nosj = uglify();
+
+console.log(nosj);
---
+++ sender/uglify.js
@@ -0,0 +1,11 @@
+/**
+ * @param json JS object to be NOSJ
+ * @returns nosj - uglified json
+ */
+const uglify = json => {
+    const nosj = 'test';
+
+    return nosj;
+};
+
+module.exports = uglify;
